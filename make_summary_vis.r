65ea1fc3cb32fa8a3a25123f1e9f13134b4ff69c
Gary Weissman <garyw@grace.pmacs.upenn.edu>
2019-12-19 10:42:58 -0500

    added some stuff

--- make_summary_vis.r
+++ make_summary_vis.r
@@ -1,1 +1,25 @@
-# This file create a visualization summary of a particular patient during  a period around the time when they might have sepsis
+# This file create a visualization summary of a particular patient 
+# during  a period around the time when they might have sepsis
+
+# -------------------------
+SEPSIS_DATA_PATH = "/data/uphs_sepsis/single_file_Sep_2019/single_file_v10-21-2019.csv"
+SEPSIS3_EVENTS = "sepsis_cases_sepsis3.csv"
+# -------------------------
+
+set.seed(24601)
+
+library(data.table)
+library(ggplot2)
+
+sep_all <- fread(SEPSIS_DATA_PATH)
+sep_events <- fread(SEPSIS3_EVENTS)
+
+
+trial_mrn <- sample(sep_events$mrn, size = 1)
+trial_ts <- sep_events[mrn == trial_mrn]
+
+make_viz <- function(MRN, center_time) {
+
+
+
+}
