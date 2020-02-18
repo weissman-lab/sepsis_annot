94413d76f57e1071a5f84b36e1d57634a6094899
Gary Weissman <garyw@grace.pmacs.upenn.edu>
2020-02-18 16:59:12 -0500

    added hover support

--- make_summary_vis.r
+++ make_summary_vis.r
@@ -14,7 +14,7 @@ set.seed(24601)
 library(data.table)
 library(ggplot2)
 library(plotly)
-library(patchwork)
+library(dplyr)
 
 sep_all <- fread(SEPSIS_DATA_PATH)
 sep_events <- fread(SEPSIS3_EVENTS)
@@ -46,68 +46,61 @@ sep_all[! is.na(as.numeric(RESPIRATORY_RATE)),
                                                                        
 # Now make a function to generate a plot for each user
 make_viz <- function(pat_mrn, onset_time) {
-
-       temp_dt <- sep_all[MRN == pat_mrn]
-       time_idx <- which(temp_dt$PD_BEG_TIMESTAMP == onset_time)
-
-       plot_title <- with(temp_dt, paste(AGE[1], 'year-old',
-                     # RACE_1[1], GENDER[1], # TOO MUCH BIAS RISK
-                     'admitted for\n',
-                     ADMIT_DX_DESC[1]))
-
-       # Randomize offsets so Sep-3 onset is at the RIGHT side
-       # This is biologically plausible based on current definition
-       # i.e. unlikely that relevant antibiotic starttsime would be 
-       # AFTER Sepsis-3 onset given organ dysfunction has already occurred
-       lb <- max(1, time_idx - 48 - round(runif(1, 2, 8)))
-       ub <- min(nrow(temp_dt), time_idx + 4 + round(runif(1, 2, 8)))
-
-       # Get range
-       temp_dt <- temp_dt[lb:ub][, sub_hour := lb:ub]
-
-       # Plot function
-       make_series <- function(dd, val) {
-                
-                # Make plot
-       	        pp <- ggplot(dd[! is.na(dd[[val]])], aes_string('sub_hour', val)) +
-	   	       geom_point() + 
-       	                geom_line(na.rm = TRUE) +
-		        theme_bw() + 
-       	                scale_x_continuous('hour', 
-       	                                   limits = c(lb, ub),
-       	                                   expand = c(.03,.03),
-       	                                   breaks = scales::pretty_breaks(n = round((ub - lb) / 10))) +
-                        scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +
-                        annotate('rect',
-                                 xmin = lb,
-                                     xmax = ub,
-                                     ymin = normal_ranges[[val]][1] , 
-                                        ymax = normal_ranges[[val]][2], 
-                                 color = 'gray', alpha = 0.2)
-       	        return(ggplotly(pp))
-        }
-
-       # List of variable names to plot in order
-       feat_vec <- c('HEART_RATE', 'SYSTOLIC_BP', 'TEMPERATURE',
-       		'RESP_RATE', 'LACTATE_RESULT')
-       
-       normal_ranges <- list(HEART_RATE = c(60, 100),
-                             SYSTOLIC_BP = c(90, 120),
-                             TEMPERATURE = c(97, 99),
-                             RESP_RATE = c(10,20),
-                             LACTATE_RESULT = c(0, 2))
-       
-       # Generate and combine plots
-       pl_lst <- lapply(feat_vec, function(val) make_series(temp_dt, val)) 
   
-       #final_plot <- wrap_elements(Reduce(`+`, pl_lst))# + 
-        #plot_layout(ncol = 1, guides = 'collect')) +
-        #ggtitle(plot_title) +
-        #theme(plot.title = element_text(hjust = 0.5))
-       print(sapply(pl_lst, class))
-        
-        subplot(pl_lst)
- }
+  temp_dt <- sep_all[MRN == pat_mrn]
+  time_idx <- which(temp_dt$PD_BEG_TIMESTAMP == onset_time)
+  
+  plot_title <- with(temp_dt, paste(AGE[1], 'year-old',
+                                    # RACE_1[1], GENDER[1], # TOO MUCH BIAS RISK
+                                    'admitted for',
+                                    ADMIT_DX_DESC[1]))
+  
+  # Randomize offsets so Sep-3 onset is at the RIGHT side
+  # This is biologically plausible based on current definition
+  # i.e. unlikely that relevant antibiotic starttsime would be 
+  # AFTER Sepsis-3 onset given organ dysfunction has already occurred
+  lb <- max(1, time_idx - 48 - round(runif(1, 2, 8)))
+  ub <- min(nrow(temp_dt), time_idx + 4 + round(runif(1, 2, 8)))
+  
+  # Get range
+  temp_dt <- temp_dt[lb:ub][, sub_hour := lb:ub]
+  
+  # List of variable names to plot in order
+  feat_vec <- c('HEART_RATE', 'SYSTOLIC_BP', 'TEMPERATURE',
+                'RESP_RATE', 'LACTATE_RESULT')
+  normal_ranges <- data.table(measure = feat_vec,
+                              norm_lo = c(60,  90,  97, 10, 0),
+                              norm_hi = c(100, 120, 99, 20, 2))
+  
+  # Prepare data
+  temp_dt <- temp_dt[, c('sub_hour', feat_vec), with = FALSE]
+  temp_dt[, hour := sub_hour][, sub_hour := NULL]
+  temp_dt_m <- melt(temp_dt, id.vars = 'hour')
+  temp_dt_m <- merge(temp_dt_m, normal_ranges, 
+                     by.x = 'variable', by.y = 'measure',
+                     all.x = TRUE)
+  
+  # Make plot
+  pp <- ggplot(temp_dt_m, aes(hour, value)) +
+    geom_line(data = temp_dt_m[! is.na(value)]) +
+    theme_bw() + 
+    scale_x_continuous('hour', 
+                       limits = c(lb, ub),
+                       expand = c(.03,.03),
+                       breaks = scales::pretty_breaks(n = round((ub - lb) / 10))) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +
+    geom_rect(aes(xmin = lb, xmax = ub, ymin = norm_lo, ymax = norm_hi),
+             color = 'gray', alpha = 0.2) +
+    geom_point() + 
+    # ^^ NB putting this point layer at the end 
+    # makes the points appear on "top" so the spike lines are more easily activated
+    facet_wrap(~ variable, ncol = 1, scales = 'free_y') +
+    ggtitle(plot_title) 
+  
+  return(ggplotly(pp) %>% 
+           layout(xaxis = list(showspikes = TRUE)) %>% 
+           config(displayModeBar = FALSE))
+}
 
 # Test it out
 images_list <- lapply(1:NUM_CASES, function(i) make_viz(cases_dt[i]$mrn, cases_dt[i]$sepsis_onset_dt))
