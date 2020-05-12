11f4bb403553831a46b7d61f299e27c3fc10d294
Gary Weissman <garyw@grace.pmacs.upenn.edu>
2020-05-12 08:46:36 -0400

    starting pilot 3

--- make_summary_vis.r
+++ make_summary_vis.r
@@ -29,7 +29,7 @@ sep_onset_dt <- sep_events[, .(onset_char_ts = dplyr::case_when(all(sofa_bcx == 
                                adm_datetime = adm_datetime[1]), 
                            by = visit]
 
-# Identify sepsis events that occur at least 72 hours into the hospitalization
+# Identify sepsis events that occur at least 48 hours into the hospitalization
 sep_onset_dt[, sepsis_onset_dt := as.POSIXct(onset_char_ts, format = '%Y-%m-%dT%H:%M:%S')]
 sep_onset_dt[, hosp_adm_dt := as.POSIXct(adm_datetime, format = '%Y-%m-%dT%H:%M:%S')]
 sep_onset_dt[, antbx_start_dt := as.POSIXct(antbx_start_char_ts, format = '%Y-%m-%dT%H:%M:%S')]
@@ -39,6 +39,12 @@ sep_onset_dt[, antbx_delay_hrs := as.numeric(antbx_start_dt - sepsis_onset_dt,
                                              units = 'hours')]
 sep_onset_dt <- sep_onset_dt[antbx_delay_hrs <= 48]
 sep_onset_dt <- sep_onset_dt[hosp_adm_to_sepsis_onset_hrs >= 48]
+
+# Generate some features
+sep_all[, adm_dt := as.POSIXct(ADMISSION_DTTM, format = '%Y-%m-%d %H:%M:%S')]
+sep_all[, disch_dt := as.POSIXct(DISCHARGE_DTTM, format = '%Y-%m-%d %H:%M:%S')]
+sep_all[, hosp_los_hrs := as.numeric(disch_dt - adm_dt, units = 'hours')]
+
 
 # Get sampling of cases
 # Per Rebecca's suggestion, sample in a stratified way across antibiotic delays
@@ -191,4 +197,102 @@ setwd('../')
 cases_dt[, png_filename := paste0(img_path, 'image', .I, '.png')]
 cases_dt[, html_filename := paste0(img_path, 'image', .I, '.html')]
 fwrite(cases_dt, 'crosswalk_sepsis.csv')
+
+# Also generate two control vignettes, i.e. those that definitely don't have sepsis
+control_visits <- sep_all[! PAT_ENC_CSN %in% sep_events$visits][hosp_los_hrs > 48]
+
+make_control_viz <- function(this_visit, onset_time, img_idx) {
+  
+  temp_dt <- sep_all[PAT_ENC_CSN == this_visit][order(PD_BEG_TIMESTAMP)]
+  time_idx <- which(temp_dt$PD_BEG_TIMESTAMP == onset_time)
+  
+  plot_title <- with(temp_dt, paste0(AGE[1], '-year-old',
+                                     # RACE_1[1], GENDER[1], # TOO MUCH BIAS RISK
+                                     ' admitted for\n',
+                                     ADMIT_DX_DESC[1]))
+  
+  # Randomize offsets so Sep-3 onset is at the RIGHT side
+  # This is biologically plausible based on current definition
+  # i.e. unlikely that relevant antibiotic starttsime would be 
+  # AFTER Sepsis-3 onset given organ dysfunction has already occurred
+  # lb <- max(1, time_idx - 48 - round(runif(1, 2, 8)))
+  # ub <- min(nrow(temp_dt), time_idx + 4 + round(runif(1, 2, 8)))
+  
+  # Based on pilot 01 results, let's keep onset right around the middle
+  lb <- max(1, time_idx - 48 - round(runif(1, 1, 6)))
+  ub <- min(nrow(temp_dt), time_idx + 48 + round(runif(1,1,6)))
+  
+  # This is hackish, but update the cases file to capture the window for each case
+  cases_dt[visit == this_visit, window_lower := lb]
+  cases_dt[visit == this_visit, window_upper := ub]
+  
+  
+  # Get range
+  temp_dt <- temp_dt[lb:ub][, sub_hour := lb:ub]
+  
+  # List of variable names to plot in order
+  feat_vec <- c('HEART_RATE', 'SYSTOLIC_BP', 'TEMPERATURE',
+                'RESP_RATE', 'LACTATE_RESULT', 'SOFA_RESP_SPO2', 
+                'creat_first_obs', 'max_WBC', 'SOFA_CNS_GLASGOW_RESULT')
+  normal_ranges <- data.table(measure = feat_vec,
+                              norm_lo = c(60,  90,  97, 10, 0, 90, 0.55, 4, 14.8),
+                              norm_hi = c(100, 120, 99, 20, 2, 100, 1.15, 11, 15.2))
+  
+  # Prepare data
+  temp_dt <- temp_dt[, c('sub_hour', feat_vec), with = FALSE]
+  temp_dt[, hour := sub_hour][, sub_hour := NULL]
+  temp_dt_m <- melt(temp_dt, id.vars = 'hour')
+  temp_dt_m <- merge(temp_dt_m, normal_ranges, 
+                     by.x = 'variable', by.y = 'measure',
+                     all.x = TRUE)
+  
+  # Fix order and labels
+  temp_dt_m[, var_fixed := factor(variable, 
+                                  levels = feat_vec,
+                                  labels = c('Heart rate', 'Systolic blood pressure',
+                                             'Temperature (F)', 'Respiratory rate',
+                                             'Lactate', 'SpO2(%)',
+                                             'Creatinine', 'White blood cell count',
+                                             'Glasgow Coma Scale'))]
+  
+  # Hlper function
+  "%nin%" <- function(x, table) !(match(x, table, nomatch = 0) > 0)
+  # NB SOFA_RESP_SPO2 is carried forward for each hour
+  # Probably only real (and new) when other vital signs are also being observed
+  # This is probably true MOST of the time
+  # Fix this to avoid signaling too much informative presence:
+  temp_dt_m <- temp_dt_m[! (variable == 'SOFA_RESP_SPO2' & 
+                              (hour %nin% temp_dt_m[variable %in% 
+                                                      c('RESP_RATE', 'SYSTOLIC_BP', 
+                                                        'TEMPERATURE')][!is.na(value)]$hour))]
+  # This is probably also true for the GCS:
+  temp_dt_m <- temp_dt_m[! (variable == 'SOFA_CNS_GLASGOW_RESULT' & 
+                              (hour %nin% temp_dt_m[variable %in% 
+                                                      c('RESP_RATE', 'SYSTOLIC_BP', 
+                                                        'TEMPERATURE')][!is.na(value)]$hour))]
+  
+  # Determine breaks
+  inc <- round((ub - lb) / 8)
+  
+  # Make plot
+  pp <- ggplot(temp_dt_m, aes(hour, value)) +
+    theme_bw() + 
+    scale_x_continuous('hour', 
+                       limits = c(lb, ub),
+                       expand = c(.03, .03),
+                       breaks = round(c(lb + 0:8 * inc)),
+                       labels = round(c(lb + 0:8 * inc - lb + 1))) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 4), expand = c(0.03, 0.03)) +
+    geom_rect(aes(xmin = lb, xmax = ub, ymin = norm_lo, ymax = norm_hi),
+              fill = 'lightgray', alpha = 0.2) +
+    geom_line(data = temp_dt_m[! is.na(value)]) +
+    geom_point() + 
+    # ^^ NB putting this point layer at the end 
+    # makes the points appear on "top" so the spike lines are more easily activated
+    facet_wrap(~ var_fixed, ncol = 1, scales = 'free_y') +
+    ggtitle(plot_title) +
+    theme(plot.title = element_text(size = 12))
+  
+  ggsave(pp, filename = paste0('images/image_control', img_idx, '.png'), width = 6, height = 8)
+}
 
